069e527f7efc7762ae803c9a8de6f7597c307564
Junior MILIEN <juniorthx@gmail.com>
2021-05-29 16:07:31 -0700

    project section and chatbox updates

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function openMsgBox(){
-    document.querySelector('.window-msg').style.opacity = '0';
+    document.querySelector('.icon-msg').style.opacity = '0';
     document.querySelector('.chatbox').style.clipPath="circle(75%";
+    document.querySelector('.container-msg').style.visibility="visible";
   }
 
   function closeChat(){
       document.querySelector('.chatbox').style.clipPath="circle(0% at 0% 95%)"
-      document.querySelector('.window-msg').style.opacity = '1';
-      document.querySelector('.window-msg').style.transitionDelay = '0.5s';
+      document.querySelector('.icon-msg').style.opacity = '1';
+      document.querySelector('.icon-msg').style.transitionDelay = '0.5s';
+      document.querySelector('.container-msg').style.visibility="hidden";
   }
